5b127423fbc2983875e74ab80b34685252ba19e3
Thiago Debia Gonçalves <thiago.goncalves@cofema.com.br>
2021-02-18 07:30:06 -0300

    Rotas criadas no arquivo web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+//adicionando o controller
+use App\Http\Controllers\Admin\TesteController;
+use function PHPUnit\Framework\isNull;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,124 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/login', function(){
+    return 'Login';
+})->name('login');
+
+
+/**
+Route::get('/admin/dashboard', function(){
+    return "Home Admin";
+})->middleware('auth');
+
+Route::get('/admin/financeiro', function(){
+    return "Financeiro Admin";
+})->middleware('auth');
+
+Route::get('/admin/produtos', function(){
+    return "Produtos Admin";
+})->middleware('auth');
+ 
+//criando um grupo de rotas, para facilitar o uso de middleware
+//primeiro as rotas que usarao o mesmo middleware
+Route::middleware([''])->group(function(){
+
+    //aqui um grupo com o mesmo prefixo
+    Route::prefix('admin')->group(function(){
+        Route::get('/dashboard', function(){
+            return "Home Admin";
+        });
+    
+        Route::get('/financeiro', function(){
+            return "Financeiro Admin";
+        });
+        
+        Route::get('/produtos', function(){
+            return "Produtos Admin";
+        });
+            //vem do controller criado com o comando
+            //php artisan make:controller Admin\TesteController
+            
+    });
+
+    
+
+    */
+
+    //aplicando middlewware, prefix e namespace para um grupo
+
+    Route::group([
+        'middleware' => [],
+        'prefix' => 'admin',
+        'namespace' => 'Admin'
+    ], function(){
+        Route::get('/recursoshumanos', [TesteController::class,'teste'])->name('rh');
+        Route::get('/controladoria', [TesteController::class,'teste'])->name('controladoria');
+        Route::get('/produtos', [TesteController::class,'teste'])->name('produtos');
+        
+    }
+);
+
+    //rota com middleware, prefixo, usando o namespace e controller
+Route::middleware([])->group(function(){
+    Route::prefix('admin')->group(function(){
+        Route::namespace('Admin')->group(function(){
+            Route::get('/dash', [TesteController::class,'teste'])->name('admin.dashboard');
+        });
+        //usando o redirecionamento com o nome da rota
+        Route::get('/', function(){
+            return redirect()->route('admin.dashboard');
+        });
+    });
+});
+    
+
+Route::get('/teste1', [TesteController::class, 'teste']);
+
+//rotas com any aceita todos os verbos http
+Route::match(['post', 'get'], '/match', function(){
+    return ('Match');
+});
+
+//rota pra uma view direta
+Route::view('/view','site.welcome');
+//redirecionando as rotas
+//primeira forma de fazer um redirect
+/* Route::get('redirect1', function(){
+    return redirect('/redirect2');
+});*/
+
+//segunda forma de fazer um redirect
+Route::redirect('redirect1','redirect2');
+Route::get('/redirect2', function () {
+    return 'Redirect 02';
+});
+//passagem de parametros na rota
+
+Route::get('/produtos/{produto?}', function($produto = ''){
+    //return ("Cliente: " . $nome);
+    return ("Produto: {$produto}");
+});
+
+Route::get('/cliente/{nome}', function($param){
+    //return ("Cliente: " . $nome);
+    return ("Cliente: {$param}");
+});
+
+Route::get('/cliente/{nome}/cadastro', function($nome){
+    //return ("Cliente: " . $nome);
+    return ("Cadastro do Cliente: {$nome}");
+});
+
+//Retorna uma página web que esta dentro da pasta view
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/teste', function(){
+    return "Olá, Página de teste";
+});
+
+Route::get( '/contato', function(){
+    return view('contato');
 });
